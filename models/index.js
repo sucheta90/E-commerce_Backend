d6aa87897de7328ac6a66d353b8aa55956753c6c
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-07-30 13:41:50 -0400

    added model attributes and constraints

--- models/index.js
+++ models/index.js
@@ -1,14 +1,21 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.hasOne(Category, {
+  foreignKey: "id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+
 
 // Tags belongToMany Products (through ProductTag)
 
